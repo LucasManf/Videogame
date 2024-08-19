f6a84c3e4ab8c7237142dd9341d7a4b3f14eb790
Lucas Manfredi <lucasmanfredij@gmail.com>
2024-08-19 18:56:51 -0300

    add enemy movement + change file names

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -11,27 +11,67 @@ public class EnemyScript : MonoBehaviour
     
     private float LastShoot;
     private int Health = 3;
+    private float startPosition;
+    public float moveSpeed; // Velocidad de movimiento del enemigo
+    private bool movingRight = true; // Dirección inicial de movimiento
+    private bool isShooting = false;
 
-    
-    // Update is called once per frame
-    private void Update()
+
+    void Start()
     {
-        if(Player==null) return;
-        Vector3 direction = Player.transform.position - transform.position; //la posicion de Player - la posicion de grunt, obtenemos el vector direccion que va de grunt a Player. si el componente x de la direcciion es positiva, el grunt tiene que mirar hacia la derecha, sino, hacia la izquierda
-        if(direction.x >=0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-        else transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        startPosition = transform.position.x;
+        movingRight = true;
+        
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Player.activeSelf) return;
 
         float distance = Mathf.Abs(Player.transform.position.x - transform.position.x);
-    
-        if(distance < 1.0f && Time.time > LastShoot + 0.7f)
+
+        if (distance < 1.0f && Time.time > LastShoot + 0.7f)
         {
-            if (Player.activeSelf)
+            isShooting = true;
+
+            Vector3 direction = Player.transform.position - transform.position;
+            if (direction.x >= 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            else transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+
+            Shoot();
+            LastShoot = Time.time;
+        }
+        else
+        {
+            isShooting = false;
+        }
+
+        if (!isShooting)
+        {
+            if (movingRight)
             {
-                Shoot();
-                LastShoot = Time.time;
+                transform.position += moveSpeed * Time.deltaTime * Vector3.right;
+            }
+            else
+            {
+                transform.position += moveSpeed * Time.deltaTime * Vector3.left;
+            }
+
+            // Invertir dirección al llegar al borde
+            if (transform.position.x > startPosition + 0.5f)
+            {
+                movingRight = false;
+                transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+            }
+            else if (transform.position.x < startPosition - 0.5f)
+            {
+                movingRight = true;
+                transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
             }
         }
     }
+
 
     private void Shoot()
     {
